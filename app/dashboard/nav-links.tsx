eaa9a52386e9ec4317725e623f7175b39a14781c
tcake <tcake@king-prawn.lol>
2025-08-25 11:49:39 +0200

    fuck you

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -29,7 +29,7 @@ export default function NavLinks() {
               },
             )}
           >
-            <LinkIcon className="w-6" />
+            <LinkIcon  />
             <p className="hidden md:block">{link.name}</p>
           </Link>
         );
